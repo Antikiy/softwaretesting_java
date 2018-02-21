f3a7ae5d471400caea42efa0408b6efe94a53d88
Antikiy <antikiy@gmail.com>
2018-02-21 21:57:01 +0200

    Added two classes "Rectangle" and "Square" with methods

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,23 +1,22 @@
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        hello("Ivan");
-        hello("Bohdan");
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    hello("Ivan");
+    hello("Bohdan");
+    System.out.println("__________");
 
-        double len = 5;
-        double len2 = 10;
-        System.out.println("Area of square is =  " + area(len));
-        System.out.println("Area of rectangle is =  " + area(len, len2));
-    }
+    double len = 5;
+    double len2 = 10;
 
-    public static void hello(String name){
-        System.out.println("Hello " + name );
-    }
+    Square s = new Square(len);
+    Rectangle r = new Rectangle(len, len2);
+    System.out.println("Area of square is =  " + s.area());
+    System.out.println("Area of rectangle is =  " + r.area());
+  }
 
-    static double area(double a){
-        return a * a;
-    }
-    static double area(double a, double b){
-        return a * b;
-    }
+  public static void hello(String name) {
+    System.out.println("Hello " + name);
+  }
+
+
 }
